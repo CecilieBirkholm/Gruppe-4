7fdd1d536d88c1738ef2f8755bf5fffe24e479c3
kacurjjj <153917404+kacurjjj@users.noreply.github.com>
2024-02-13 11:36:15 +0100

    js html css

--- background.js
+++ background.js
@@ -1,4 +1,16 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("light-mode");
-}
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var section1Height = document.querySelector('.header-biside').offsetHeight;
+    var section2Height = section1Height + document.querySelector('.om-mig-side').offsetHeight;
+
+    if (scrollPosition < section1Height) {
+        document.body.classList.remove('section2', 'section3');
+        document.body.classList.add('section1');
+    } else if (scrollPosition >= section1Height && scrollPosition < section2Height) {
+        document.body.classList.remove('section1', 'section3');
+        document.body.classList.add('section2');
+    } else {
+        document.body.classList.remove('section1', 'section2');
+        document.body.classList.add('section3');
+    }
+});
