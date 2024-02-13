1040da00a3616e22ad20897e99e62cb49ec4af7a
kacurjjj <153917404+kacurjjj@users.noreply.github.com>
2024-02-13 09:56:06 +0100

    JS HTML CSS

--- background.js
+++ background.js
@@ -1,12 +1,4 @@
-const toggle = document.getElementById('toggle');
-const body = document.body;
-
-toggle.addEventListener('input', e =>{
-    const isChecked = e.target.checked;
-
-    if (isChecked) {
-        body.classList.add('dark-theme');
-    } else {
-        body.classList.remove('dark-theme');
-    }
-});
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+}
